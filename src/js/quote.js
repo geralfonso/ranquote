f1ba586d8831468f97a5e652782bec6c6cad95fe
German Alfonso <geralfonsocastillo@gmail.com>
2018-03-30 10:39:52 -0500

    db updated, adding giphy url to all gifs

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
-  var colors = ["22,160,133", "39,174,96", "44,62,80", "243,156,18", "231,76,60", "155,89,182", "251,105,100", "52,34,36", "#472E32", "#BDBB99", "#77B1A9", "#73A857"],
+  var colors = ["22,160,133", "39,174,96", "44,62,80", "243,156,18", "231,76,60", "155,89,182", "251,105,100", "52,34,36", "71,46,50", "189,187,153", "119,177,169", "115,168,87", "0,51,102", "128,0,0", "0,206,209"],
     cite = $('.quote_cite'),
     img = $('.quote_img img'),
     author = $('.quote_autor, .title_heading'),
     name = $('.title_heading'),
     btnColor = $('.social_tittle, .quote_btn, .social_buttons'),
     body = $('body');
+
+  $.preloadImages("images/nav/1.jpg", "images/nav/2.jpg")
 
   function change(quote) {
     var current = Math.floor(Math.random() * (quote.length));
@@ -28,13 +30,6 @@ $(document).ready(function () {
         el.preventDefault();
         change(curQuote);
       });
-    },
-    error: function (request, errorType, errorMessage) {
-      console.log('Error: ' + errorType + ' with message: ' + errorMessage);
-    },
-    timeout: 1000,
-    beforeSend: function () {
-      $('.confirmation').addClass('is--loading');
     }
   });
 
