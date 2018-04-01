9df0e61f8eb7eb286dcf8944ef0669549c459ce8
German Alfonso <geralfonsocastillo@gmail.com>
2018-04-01 14:07:26 -0500

    db updated, updating gifs for ELon y Da Vinci

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  var colors = ["22,160,133", "39,174,96", "44,62,80", "243,156,18", "231,76,60", "155,89,182", "251,105,100", "52,34,36", "71,46,50", "189,187,153", "119,177,169", "115,168,87", "0,51,102", "128,0,0", "0,206,209"],
-    cite = $('.quote_cite'),
+  var cite = $('.quote_cite'),
     img = $('.quote_img img'),
     author = $('.quote_autor, .title_heading'),
     name = $('.title_heading'),
@@ -16,10 +15,10 @@ $(document).ready(function () {
       'alt': quote[current].author
     });
     author.text(quote[current].author);
-    body.css('background-color', 'rgba(' + colors[current] + ',0.3)');
-    cite.css('background-color', 'rgb(' + colors[current] + ')');
-    name.css('color', 'rgb(' + colors[current] + ')');
-    btnColor.css('color', 'rgb(' + colors[current] + ')');
+    body.css('background-color', 'rgba(' + quote[current].color + ',0.3)');
+    cite.css('background-color', 'rgb(' + quote[current].color + ')');
+    name.css('color', 'rgb(' + quote[current].color + ')');
+    btnColor.css('color', 'rgb(' + quote[current].color + ')');
   }
   $.ajax("https://geralfonso.github.io/ranquote/server/db.json", {
     success: function (json) {
--- src/js/quote.js
+++ src/js/quote.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  var colors = ["22,160,133", "39,174,96", "44,62,80", "243,156,18", "231,76,60", "155,89,182", "251,105,100", "52,34,36", "71,46,50", "189,187,153", "119,177,169", "115,168,87", "0,51,102", "128,0,0", "0,206,209"],
-    cite = $('.quote_cite'),
+  var cite = $('.quote_cite'),
     img = $('.quote_img img'),
     author = $('.quote_autor, .title_heading'),
     name = $('.title_heading'),
