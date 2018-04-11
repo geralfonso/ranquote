ca665df23b13a0977886a2ac305a26917e3fcab1
German Alfonso <geralfonsocastillo@gmail.com>
2018-04-11 13:03:56 -0500

    Primer envio desde la eliminacion

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+"use strict";
+
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync').create(),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  pump = require('pump'),
+  uglify = require('gulp-uglify'),
+  pug = require('gulp-pug');
+//sassdoc = require('sassdoc'),
+//rename = require('gulp-rename'),
+//converter = require('sass-convert');
+
+// gulp.task('pug', () => {
+//   gulp.src('./src/html/*.pug')
+//     .pipe(pug({
+//       pretty: true
+//     }))
+//     .pipe(gulp.dest('./docs/'))
+//     .pipe(browserSync.reload({
+//       stream: true
+//     }));
+// });
+
+// convertir scss en sass
+gulp.task('sassdoc', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(converter({
+      from: 'scss',
+      to: 'sass',
+    }))
+    .pipe(sassdoc())
+    .pipe(rename((path) => {
+      path.extname = ".sass";
+    }))
+    .pipe(gulp.dest('./src/sass/'));
+});
+
+
+gulp.task('pug', (pg) => {
+  pump([
+      gulp.src('./src/html/*.pug'),
+      pug({
+        pretty: true
+      }),
+      gulp.dest('./docs/'),
+      browserSync.reload({
+        stream: true
+      })
+    ],
+    pg
+  );
+
+});
+
+gulp.task('sass', () => {
+  gulp.src(['./src/sass/*.sass', './src/sass/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      sourceComments: false
+    })).on('error', sass.logError)
+    .pipe(autoprefixer({
+      versions: ['last 10 versions']
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./docs/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+gulp.task('compress', (cb) => {
+  pump([
+      gulp.src(['./src/js/*.js']),
+      concat('bundle.js'),
+      gulp.dest('./docs/js')
+    ],
+    cb
+  );
+
+});
+
+
+
+gulp.task('js-watch', ['compress'], (done) => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('server', () => {
+  browserSync.init({
+    port: '3000',
+    server: {
+      baseDir: './docs/'
+    }
+  });
+});
+
+gulp.task('default', ['server'], () => {
+  gulp.watch(['./src/html/*.pug', './src/html/**/*.pug'], ['pug']);
+  gulp.watch(['./src/sass/*.sass', './src/sass/**/*.sass'], ['sass']);
+  gulp.watch('./src/js/*.js', ['js-watch']);
+  gulp.watch('./docs/index.html').on('change', browserSync.reload);
+});
---
+++ src/js/quote.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  var cite = $('.quote_cite'),
+    img = $('.quote_img img'),
+    author = $('.quote_autor, .title_heading'),
+    name = $('.title_heading'),
+    btnColor = $('.social_tittle, .quote_btn, .social_buttons'),
+    body = $('body'),
+    twitter = $('.twitter--hover');
+
+  function change(quote) {
+    var newarr = [],
+      current,
+      ran;
+
+
+    current = Math.floor(Math.random() * (quote.length));
+    newarr.push(current);
+
+    console.log(current);
+
+    cite.find('cite').text(quote[current].cite);
+    img.attr({
+      'src': quote[current].image,
+      'alt': quote[current].author
+    });
+    author.text(quote[current].author);
+    body.css('background-color', 'rgba(' + quote[current].color + ',0.3)');
+    cite.css('background-color', 'rgb(' + quote[current].color + ')');
+    name.css('color', 'rgb(' + quote[current].color + ')');
+    btnColor.css('color', 'rgb(' + quote[current].color + ')');
+    twitter.attr('href', `http://twitter.com/share?text=${quote[current].cite} -${quote[current].author}&via=@GerAlfonso&url=https%3A%2F%2Fgeralfonso.github.io/ranquote`);
+
+  }
+  $.ajax("https://geralfonso.github.io/ranquote/server/db.json", {
+    dataType: 'json',
+    success: function (json) {
+      var curQuote = json.quotes;
+      change(curQuote);
+      $('.quote_btn').on('click', function (el) {
+        el.preventDefault();
+        change(curQuote);
+      });
+    }
+  });
+});
