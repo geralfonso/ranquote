d2987ee5d64d7a5533595b775d8c65f657f779ee
German Alfonso <geralfonsocastillo@gmail.com>
2018-04-13 11:40:24 -0500

    facebook share 1.2

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -52,13 +52,3 @@ $(document).ready(function () {
     }
   });
 });
-$(document).ready(function () {
-  var curTitle = $('.quote_autor').text(),
-    curDescription = $('.quote_cite').find('cite').text(),
-    curImage = $('.quote_img').find('img').attr('src');
-  $('.social_buttons').on('click', '.facebook--hover', function (el) {
-    el.preventDefault();
-
-    shareOverrideOGMeta(curTitle, curDescription, curImage);
-  });
-});
